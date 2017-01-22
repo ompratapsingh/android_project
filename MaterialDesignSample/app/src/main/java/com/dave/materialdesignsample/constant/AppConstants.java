1e6131c518fe1fd1de96cb26a4eb508d75ae9204
ompratapsingh <pratapomsingh@outlook.com>
2017-01-22 21:57:57 +0530

    Updated 22-01-2017 [om]

--- MaterialDesignSample/app/src/main/java/com/dave/materialdesignsample/constant/AppConstants.java
+++ MaterialDesignSample/app/src/main/java/com/dave/materialdesignsample/constant/AppConstants.java
@@ -9,6 +9,9 @@ public interface AppConstants {
    // Registration URL
    String APPS_REGISTRATION_ACTION_ID="1";
    String REGISTRATION_URL = "";
+    String REGISTER_ACTION_ID="1";
+    String SAVE_WISHES_ACTION_ID="2";
+    String GET_WISHES_ACTION_ID="3";
 
     // Param for Registration
     String R_NAME ="name";
